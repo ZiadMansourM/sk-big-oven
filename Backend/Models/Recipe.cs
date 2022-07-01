5b61297017964aadef1ae356383c3ad4c53827db
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-07-01 17:03:57 +0200

    ✅ ADD: Recipe model

---
+++ Backend/Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.Models;
+
+public class Recipe
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public List<string> Ingredients { get; set; }
+    public List<string> Instructions { get; set; }
+    public List<Category> Categories { get; set; }
+
+    public Recipe(string name, List<string> ingredients, List<string> instructions, List<Category> categories)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Ingredients = ingredients;
+        Instructions = instructions;
+        Categories = categories;
+    }
+}
