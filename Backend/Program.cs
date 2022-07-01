0d1154058a3c95c25e9539ec3b2ed1f708f30de5
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-07-01 17:37:03 +0200

    🫡 DONE: run first test successfully

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(value: "Hello, World!");
 
+var service = new Backend.Controller.Services.JsonService();
+var controller = new Backend.Controller.Controller(service);
+var viewService = new Backend.Views.Services.ConsoleService();
+var router = new Backend.Views.Router(controller, viewService);
+
+router.Run();
