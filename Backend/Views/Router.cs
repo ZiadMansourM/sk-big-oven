05c147ab457298034a555e77055e01d5c4c17ba3
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-07-01 17:32:45 +0200

    ✅ ADD: boilerplate of router class

--- Backend/Views/Router.cs
+++ Backend/Views/Router.cs
@@ -2,8 +2,20 @@
 
 public class Router
 {
-    public Router()
+    private readonly Controller.Controller _controller;
+    private readonly Services.IViewService _service;
+
+    public Router(Controller.Controller controller, Services.IViewService service)
     {
+        ArgumentNullException.ThrowIfNull(controller);
+        ArgumentNullException.ThrowIfNull(service);
+        _controller = controller;
+        _service = service;
+    }
+
+    public void Run()
+    {
+        Console.WriteLine(value: "Good Job ^^");
     }
 }
 
