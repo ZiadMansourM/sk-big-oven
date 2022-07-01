c92c943f1f5ab0de36cf32ac6b625c0480179512
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-07-01 17:04:16 +0200

    ✅ ADD: Category model

---
+++ Backend/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Models;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+
+    public Category(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+    }
+}
