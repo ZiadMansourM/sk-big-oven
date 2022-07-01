f966a5d26ef2afb5ecbec8edf4ba824a19ca08df
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-07-01 21:09:48 +0200

    ✅ DONE: boilerplate for console flow

--- Backend/Views/Services/ConsoleService.cs
+++ Backend/Views/Services/ConsoleService.cs
@@ -1,18 +1,26 @@
-﻿using Spectre.Console;
-namespace Backend.Views.Services;
+﻿namespace Backend.Views.Services;
 
 public class ConsoleService : IViewService
 {
     private readonly Controller.Controller _controller;
+    private Dictionary<string, Action> _modeSelector;
 
     public ConsoleService(Controller.Controller controller)
     {
         _controller = controller;
+        _modeSelector = new Dictionary<string, Action>
+        {
+            ["Recipes"] = Davinci.RecipesMain,
+            ["Categories"] = Davinci.CategoriesMain,
+            ["Exit"] = () => Environment.Exit(0),
+        };
     }
 
     public void Run()
     {
-        AnsiConsole.Markup("[underline red]Hello[/] World!");
+        Davinci.Setup("BigOven ...");
+        Davinci.WriteDivider("Talk to me ^^");
+        while (true)
+            _modeSelector[Davinci.GetMode()]();
     }
 }
-
---
+++ Backend/Views/Services/Davinci.cs
@@ -0,0 +1,65 @@
+﻿using Spectre.Console;
+namespace Backend.Views.Services;
+
+public class Davinci
+{
+    public static void Setup(string name)
+    {
+        AnsiConsole.Write(
+            new Panel(
+                new FigletText(name)
+                .Centered()
+                .Color(Color.Red)
+            ).RoundedBorder()
+        );
+    }
+
+    public static void WriteDivider(string text)
+    {
+        AnsiConsole.WriteLine();
+        AnsiConsole.Write(
+            new Rule($"[yellow]{text}[/]")
+            .RuleStyle("grey")
+            .LeftAligned()
+        );
+    }
+
+    public static string GetMode()
+    {
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("What do you [green] want to do today ?[/]")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Move up and down to reveal more Modes)[/]")
+            .AddChoices(new[] {
+                "Recipes", "Categories", "Exit"
+            })
+        );
+    }
+
+    public static void RecipesMain()
+    {
+         string request = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("About recipes, [green] pick what want?[/]")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Move up and down to reveal more Options)[/]")
+            .AddChoices(new[] {
+                "List", "Get", "Create", "Update", "Delete"
+            })
+        );
+    }
+
+    public static void CategoriesMain()
+    {
+        string request = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("About categories, [green] pick what want?[/]")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Move up and down to reveal more Options)[/]")
+            .AddChoices(new[] {
+                "List", "Get", "Create", "Update", "Delete"
+            })
+        );
+    }
+}
