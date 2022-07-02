0b4b6d77ce6e90aa7d020ae2e59ed7afd38c8a23
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-07-02 02:33:46 +0200

    👀 RUN: small refactor

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,4 @@
-﻿Console.WriteLine(value: "Hello, World!");
-
-var service = new Backend.Controller.Services.JsonService();
+﻿var service = new Backend.Controller.Services.JsonService();
 var controller = new Backend.Controller.Controller(service);
 var viewService = new Backend.Views.Services.ConsoleService(controller);
 var router = new Backend.Views.Router(viewService);
